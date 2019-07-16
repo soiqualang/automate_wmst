375ea20d88534c66abd2b633197e49e6bd1aa935
soiqualang <soiqualang.chentreu@gmail.com>
2019-07-16 15:27:49 +0700

    update new services

--- redrose.js
+++ redrose.js
@@ -1,12 +1,15 @@
 var map = L.map('map', {
-    zoom: 5,
+    zoom: 12,
     zoomDelta: 0.5,
     zoomSnap: 0.5,    
     fullscreenControl: true,
     timeDimension: true,
     timeDimensionControl: true,
-    center: [15.46,106.64],
+    center: [10.9978,106.5801],
 });
+
+var hash = new L.Hash(map);
+
 //106.64,15.46
 
 // https://ogcie.iblsoft.com/metocean/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities
@@ -14,6 +17,18 @@ var map = L.map('map', {
 
 var atlas_vietnamWMS = "http://dev.dothanhlong.org:8080/geoserver/atlas_vietnam/wms";
 var testWMS = "https://ogcie.iblsoft.com/metocean/wms";
+var wmst_serv='http://girs.vn:8081/geoserver15/automate/wms';
+
+var proxy = 'proxy.php';
+
+
+var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}), streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr});
+
+grayscale.addTo(map);
 
 var baseLayer=L.tileLayer.wms(testWMS, {
     layers: 'foreground-lines',
@@ -21,56 +36,50 @@ var baseLayer=L.tileLayer.wms(testWMS, {
     transparent: true,
     crs: L.CRS.EPSG4326
 });
-var vn_ub=L.tileLayer.wms(atlas_vietnamWMS, {
-    layers: 'atlas_vietnam:1vn_ub',
-    format: 'image/png',
-    transparent: true,
-    crs: L.CRS.EPSG4326
-});
-var vn_tinh_hsts=L.tileLayer.wms(atlas_vietnamWMS, {
-    layers: 'atlas_vietnam:1vn_tinh_hsts',
-    format: 'image/png',
-    transparent: true,
-    crs: L.CRS.EPSG4326
-});
-var vn_tinh=L.tileLayer.wms(atlas_vietnamWMS, {
-    layers: 'atlas_vietnam:vn_tinh',
-    format: 'image/png',
-    transparent: true,
-    crs: L.CRS.EPSG4326
-});
+//baseLayer.addTo(map);
 
 
-var testLayer = L.tileLayer.wms(atlas_vietnamWMS, {
-        layers: 'timeline_georeferencer',
+
+
+var LST_source = L.tileLayer.wms(wmst_serv, {
+        layers: 'lst',
     format: 'image/png',
     transparent: true,
 	crs: L.CRS.EPSG4326,
     time: '0001-01-01T00:00:00'
-    //opacity: 0.3,
-    //attribution: 'hahahaha'
 });
 
-
-var proxy = 'proxy.php';
-var testTimeLayer = L.timeDimension.layer.wms(testLayer, {
+var LST = L.timeDimension.layer.wms(LST_source, {
     proxy: proxy,
     updateTimeDimension: true,
 });
-testTimeLayer.addTo(map);
+//LST.addTo(map);
 
-baseLayer.addTo(map);
+
+var NDVI_source = L.tileLayer.wms(wmst_serv, {
+        layers: 'ndvi',
+    format: 'image/png',
+    transparent: true,
+	crs: L.CRS.EPSG4326,
+    time: '0001-01-01T00:00:00'
+});
+
+var NDVI = L.timeDimension.layer.wms(NDVI_source, {
+    proxy: proxy,
+    updateTimeDimension: true,
+});
+//NDVI.addTo(map);
+
+
 
 var baseMaps = {
-    "Lớp nền": baseLayer
+    "Lớp nền": grayscale
 };
 
 var overlayMaps = {
-    "Lãnh thổ Việt Nam": testTimeLayer,
-    "Tỉnh thành":vn_tinh_hsts,
-    "Lớp ranh thế giới": baseLayer,
-    "Ranh giớiTỉnh":vn_tinh,
-    "Ủy ban":vn_ub
+    "LST": LST,
+	"NDVI":NDVI,
+    "Lớp ranh thế giới": baseLayer
 };
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
